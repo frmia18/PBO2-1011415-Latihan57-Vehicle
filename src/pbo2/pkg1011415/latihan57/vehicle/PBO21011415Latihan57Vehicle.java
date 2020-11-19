d6c584c2cf2ba34d151a2e5dabdd8e98b9b40fd9
frmia18 <frmiafm@gmail.com>
2020-11-19 18:59:07 +0700

    pbo 2-10116415-latihan 57

---
+++ src/pbo2/pkg1011415/latihan57/vehicle/PBO21011415Latihan57Vehicle.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg1011415.latihan57.vehicle;
+
+/**
+ *
+ * @author Server SMKS AL QUDSY
+ */
+public class PBO21011415Latihan57Vehicle {
+
+ /**
+ *Nama  : MIA AISYAH F
+ *Kelas : PBO2
+ *NIM   : 10116415
+ *Deskripso Program : program ini berisi program yang menampilkan konstruktor
+ *                    dari Vehicle
+ */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          Bicycle bicycle = new Bicycle();
+        Skateboard skateboard = new Skateboard();
+    }
+    
+}
